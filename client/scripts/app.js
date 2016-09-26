20796228f33891068304e48c5f227db88675fc96
tcwood <travis.cwood@gmail.com>
2016-09-26 16:39:58 -0700

    Escape xss attacks and remove nodes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,9 +7,9 @@ App.prototype.init = function() {};
 
 
 var message = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan'
+  username: 'Jace',
+  text: 'sleep hard, lift hard',
+  roomname: 'lobby'
 };
 
 App.prototype.send = function(message) {
@@ -29,17 +29,18 @@ App.prototype.send = function(message) {
   });
 }; 
 
-App.prototype.fetch = function() {
-  // var context = this; 
+App.prototype.fetch = function(roomname = 'lobby') {
+  
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
     type: 'GET',
+    //we haven't filtered by roomname
+    data: {'order': '-createdAt'},
     contentType: 'application/json',
     success: data => {
       // iterate through each object in data message and call render message on each one
       data.results.forEach( value => {
-        console.log(this);
         this.renderMessage(value);
       });
       //can use two this bind's here if we want because we understand bind now watchout
@@ -54,11 +55,15 @@ App.prototype.fetch = function() {
 
 App.prototype.renderMessage = function(message) {
   // add a message to the dom
-  $('#chats').append('<li>' + message.username + ': ' + message.text + '</li>');
+  var origUser = message.username || '';
+  var origText = message.text || '';
+  var user = origUser.replace(/</g, '&lt;').replace(/>/g, '&gt; ');
+  var text = origText.replace(/</g, '&lt;').replace(/>/g, '&gt; ');
+  $('#chats').append('<li>' + user + ': ' + text + '</li>');
 };
 
 App.prototype.clearMessages = function() {
-
+  $('#chats').empty();
 };
 
 // Retrieve messages from Parse
@@ -79,6 +84,7 @@ var app = new App();
 
 app.send(message);
 app.fetch();
+
 
 //   https://api.parse.com/1/classes/messages
 
